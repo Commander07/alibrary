12c1c019298a469c310474d811b0ca6401d62290
Commander <email>
2023-02-17 22:05:55 +0100

    Easier imports

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .aimage import aimage  # noqa: F401
+from .alogger import alogger  # noqa: F401
+from .aparser import aparser  # noqa: F401
+from .ascraper import ascraper  # noqa: F401
--- alogger/alogger.py
+++ alogger/alogger.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import json
+import os
+import time
 from enum import Enum, auto
 from typing import Any
 
 import __main__
-
 
 __loggers__: dict[str, Logger] = {}
 
@@ -19,10 +21,57 @@ class Level(Enum):
     FATAL = auto()
 
 
+class __config__:
+    def __init__(self) -> None:
+        if os.path.exists("alogger.json"):
+            pass
+        self.filename: str | None = None
+        self.format: str = ""
+        self.datefmt: str = ""
+        self.sep: str = " "
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        super().__setattr__(__name, __value)
+        with open("alogger.json", "w") as f:
+            json.dump(self, f)
+
+
+__config_obj__ = __config__()
+
+
 def getLogger(name: str | None = None) -> Logger:
     if name:
         return __loggers__.get(name, Logger(name))
-    return __loggers__.get(__main__.__file__, Logger(__main__.__file__))
+    if __main__.__file__ in __loggers__.keys():
+        return __loggers__[__main__.__file__]
+    logger = Logger(__main__.__file__)
+    __loggers__[__main__.__file__] = logger
+    return logger
+
+
+def basicConfig(
+    filename: str | None = None,
+    format: str = "[{time}] [{name}] [{levelname}] {message}",
+    datefmt: str = "%H:%M:%S",
+    sep: str = " ",
+) -> None:
+    config(filename=filename, format=format, datefmt=datefmt, sep=sep)
+
+
+def config(
+    filename: str | None = None,
+    format: str | None = None,
+    datefmt: str | None = None,
+    sep: str | None = None,
+) -> None:
+    if filename:
+        __config_obj__.filename = filename
+    if format:
+        __config_obj__.format = format
+    if datefmt:
+        __config_obj__.datefmt = datefmt
+    if sep:
+        __config_obj__.sep = sep
 
 
 class Logger:
@@ -33,12 +82,41 @@ class Logger:
         filename: str | None = None,
         format: str = "[{time}] [{name}] [{levelname}] {message}",
         datefmt: str = "%H:%M:%S",
+        sep: str = " ",
     ) -> None:
         self.name = name
         self.level = level
         self.filename = filename
         self.format = format
         self.datefmt = datefmt
+        self.sep = sep
 
-    def _log(self, level: Level, msg: str, *obj: Any) -> None:
-        pass
+    def _log(self, level: Level, *msg: str) -> None:
+        message = self.format.format(
+            time=time.strftime(self.datefmt),
+            name=self.name,
+            levelname=level.name,
+            message=self.sep.join(msg),
+        )
+        if self.filename:
+            with open(self.filename, "a") as f:
+                f.write(message)
+        print(message)
+
+    def trace(self, *msg: str) -> None:
+        self._log(Level.TRACE, *msg)
+
+    def debug(self, *msg: str) -> None:
+        self._log(Level.DEBUG, *msg)
+
+    def info(self, *msg: str) -> None:
+        self._log(Level.INFO, *msg)
+
+    def warning(self, *msg: str) -> None:
+        self._log(Level.WARNING, *msg)
+
+    def error(self, *msg: str) -> None:
+        self._log(Level.ERROR, *msg)
+
+    def fatal(self, *msg: str) -> None:
+        self._log(Level.FATAL, *msg)
